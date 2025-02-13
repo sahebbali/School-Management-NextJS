66fb2611c9a566eb1ad6c9bdab1a83e5ea14d57b
sahebbali <saheb1996jmr@gmail.com>
2025-02-13 21:28:50 +0600

    add pagination

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -143,7 +143,8 @@ const TeacherListPage = async ({
     }),
     prisma.teacher.count({ where: query }),
   ]);
-
+  console.log({ data });
+  console.log({ count });
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
