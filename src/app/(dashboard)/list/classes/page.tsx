1889ccb189d556c9cbcd02fd67d4e9641184b030
sahebbali <saheb1996jmr@gmail.com>
2025-02-24 16:29:43 +0600

    fixed some issue

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -117,7 +117,7 @@ const renderRow = (item: ClassList) => (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Classes</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Classes {`(${count})`}</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
